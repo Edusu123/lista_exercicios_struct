09f9c7faab4169935da93d1b8741ae5ac867fdbf
Eduardo Conde Pires <eduardo.pires@threeo.com.br>
2022-11-17 21:21:59 -0300

    Exercício 12

--- ex8_.c
+++ ex8_.c
@@ -10,7 +10,7 @@
 #define LIMITE_FUNC 100
 #define LIMITE_RG 10
 #define LIMITE_NUM 10
-#define QTD_MENU 2
+#define QTD_MENU 4
 
 typedef struct
 {
@@ -38,6 +38,8 @@ typedef struct
 TipoCadastro IniciaCadastro();
 TipoCadastro LeFuncionarios(TipoCadastro atual);
 void ListaFuncionarios(TipoCadastro listagem);
+void OrdenaNome(TipoCadastro listagem);
+void OrdenaSalario(TipoCadastro listagem);
 void limpaBuffer();
 
 int main(void)
@@ -45,18 +47,55 @@ int main(void)
     TipoCadastro cadastro = IniciaCadastro();
     setlocale(LC_ALL, "Portuguese");
 
-    char *menu[QTD_MENU] = {"Cadastro", "Listagem"};
+    char *menu[QTD_MENU] = {"Cadastro", "Listagem", "Ordenar por nome", "Ordenar por salário"};
     int dec;
 
-    strcpy(cadastro.funcionarios[0].nome, "Eduardo Conde Pires");
+    // dados para teste//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    strcpy(cadastro.funcionarios[0].nome, "Eduardo");
     strcpy(cadastro.funcionarios[0].rg, "52.900.680-7");
     cadastro.funcionarios[0].salario = 12000.0;
+    cadastro.funcionarios[0].idade = 19;
     cadastro.funcionarios[0].sexo = 'm';
     cadastro.funcionarios[0].dataNascimento.dia = 6;
     cadastro.funcionarios[0].dataNascimento.mes = 2;
     cadastro.funcionarios[0].dataNascimento.ano = 2003;
 
     cadastro.quant++;
+
+    strcpy(cadastro.funcionarios[1].nome, "Matheus");
+    strcpy(cadastro.funcionarios[1].rg, "52.900.680-8");
+    cadastro.funcionarios[1].salario = 13000.0;
+    cadastro.funcionarios[1].idade = 20;
+    cadastro.funcionarios[1].sexo = 'm';
+    cadastro.funcionarios[1].dataNascimento.dia = 6;
+    cadastro.funcionarios[1].dataNascimento.mes = 2;
+    cadastro.funcionarios[1].dataNascimento.ano = 2004;
+
+    cadastro.quant++;
+
+    strcpy(cadastro.funcionarios[2].nome, "Amauri");
+    strcpy(cadastro.funcionarios[2].rg, "52.900.680-9");
+    cadastro.funcionarios[2].salario = 11000.0;
+    cadastro.funcionarios[2].idade = 21;
+    cadastro.funcionarios[2].sexo = 'm';
+    cadastro.funcionarios[2].dataNascimento.dia = 6;
+    cadastro.funcionarios[2].dataNascimento.mes = 2;
+    cadastro.funcionarios[2].dataNascimento.ano = 2005;
+
+    cadastro.quant++;
+
+    strcpy(cadastro.funcionarios[3].nome, "Patricia");
+    strcpy(cadastro.funcionarios[3].rg, "52.900.681-0");
+    cadastro.funcionarios[3].salario = 14000.0;
+    cadastro.funcionarios[3].idade = 22;
+    cadastro.funcionarios[3].sexo = 'f';
+    cadastro.funcionarios[3].dataNascimento.dia = 6;
+    cadastro.funcionarios[3].dataNascimento.mes = 2;
+    cadastro.funcionarios[3].dataNascimento.ano = 2006;
+
+    cadastro.quant++;
+
+    // dados para teste//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     do
     {
@@ -70,6 +109,12 @@ int main(void)
             break;
         case 1:
             ListaFuncionarios(cadastro);
+            break;
+        case 2:
+            OrdenaNome(cadastro);
+            break;
+        case 3:
+            OrdenaSalario(cadastro);
             break;
         default:
             break;
@@ -92,7 +137,7 @@ TipoCadastro LeFuncionarios(TipoCadastro atual)
     int qtdFuncionarios;
     cursorVisivel(true);
     gotoxy(3, 5);
-    printf("Quantidade de funcionários a serem cadastrados: ");
+    printf("Quantidade de funcionï¿½rios a serem cadastrados: ");
     fgets(numStr, sizeof numStr, stdin);
     qtdFuncionarios = atoi(numStr);
 
@@ -102,10 +147,10 @@ TipoCadastro LeFuncionarios(TipoCadastro atual)
         system("cls");
 
         gotoxy(3, 5);
-        printf("Funcionário %02d", atual.quant + 1);
+        printf("Funcionï¿½rio %02d", atual.quant + 1);
 
         gotoxy(3, 7);
-        printf("Nome do funcionário: ");
+        printf("Nome do funcionï¿½rio: ");
         fgets(novoFuncionario.nome, sizeof novoFuncionario.nome, stdin);
 
         gotoxy(3, 8);
@@ -113,7 +158,7 @@ TipoCadastro LeFuncionarios(TipoCadastro atual)
         fgets(novoFuncionario.rg, sizeof novoFuncionario.rg, stdin);
 
         gotoxy(3, 9);
-        printf("Salário: ");
+        printf("Salï¿½rio: ");
         limpaBuffer();
         fgets(numStr, sizeof numStr, stdin);
         novoFuncionario.salario = atof(numStr);
@@ -124,7 +169,7 @@ TipoCadastro LeFuncionarios(TipoCadastro atual)
         novoFuncionario.idade = atoi(numStr);
 
         gotoxy(3, 11);
-        printf("Gênero (m/f/n/o): ");
+        printf("Gï¿½nero (m/f/n/o): ");
         novoFuncionario.sexo = fgetc(stdin);
 
         gotoxy(3, 12);
@@ -135,7 +180,7 @@ TipoCadastro LeFuncionarios(TipoCadastro atual)
         fgets(numStr, sizeof numStr, stdin);
         novoFuncionario.dataNascimento.dia = atoi(numStr);
         gotoxy(5, 14);
-        printf("Mês: ");
+        printf("Mï¿½s: ");
         fgets(numStr, sizeof numStr, stdin);
         novoFuncionario.dataNascimento.mes = atoi(numStr);
         gotoxy(5, 15);
@@ -163,7 +208,7 @@ void ListaFuncionarios(TipoCadastro listagem)
     }
 
     gotoxy(3, 3);
-    printf("Lista de funcionários");
+    printf("Lista de funcionï¿½rios");
 
     const int colunaNome = 3, colunaRG = 25, colunaSalario = 40, colunaIdade = 55, colunaSexo = 65, colunaDataNascimento = 70;
 
@@ -210,6 +255,58 @@ void ListaFuncionarios(TipoCadastro listagem)
 
     gotoxy(3, 28);
     system("pause");
+}
+
+void OrdenaNome(TipoCadastro listagem)
+{
+    TipoCadastro ordenado;
+    ordenado.quant = listagem.quant;
+
+    for (int i = 0; i < listagem.quant; i++)
+    {
+        ordenado.funcionarios[i] = listagem.funcionarios[i];
+    }
+
+    for (int i = 0; i < ordenado.quant; i++)
+    {
+        for (int j = 0; j < ordenado.quant - i - 1; j++)
+        {
+            if (strcmp(ordenado.funcionarios[j].nome, ordenado.funcionarios[j + 1].nome) > 0)
+            {
+                TipoReg aux = ordenado.funcionarios[j];
+                ordenado.funcionarios[j] = ordenado.funcionarios[j + 1];
+                ordenado.funcionarios[j + 1] = aux;
+            }
+        }
+    }
+
+    ListaFuncionarios(ordenado);
+}
+
+void OrdenaSalario(TipoCadastro listagem)
+{
+    TipoCadastro ordenado;
+    ordenado.quant = listagem.quant;
+
+    for (int i = 0; i < listagem.quant; i++)
+    {
+        ordenado.funcionarios[i] = listagem.funcionarios[i];
+    }
+
+    for (int i = 0; i < ordenado.quant; i++)
+    {
+        for (int j = 0; j < ordenado.quant - i - 1; j++)
+        {
+            if (ordenado.funcionarios[j].salario > ordenado.funcionarios[j + 1].salario)
+            {
+                TipoReg aux = ordenado.funcionarios[j];
+                ordenado.funcionarios[j] = ordenado.funcionarios[j + 1];
+                ordenado.funcionarios[j + 1] = aux;
+            }
+        }
+    }
+
+    ListaFuncionarios(ordenado);
 }
 
 void limpaBuffer()
